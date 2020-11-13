300813466cb07175f7bd80ac26ca69bfc83f4a93
Cof <example&example.com>
2020-11-13 01:13:03 +0100

    running light and blinking add

--- README.md
+++ README.md
@@ -14,12 +14,12 @@ START
 1. A gyűrű fele az egyik játékos a másik fele a másik járékos színével világít.
 2. A kezdő játékos szinei párszor villognak
 3. A gyűrű szinei lekapcsolnak
-4. 12 óránál egy led sárgán világít
+4. 12 és 8 óránál egy led zölden világít
 5. Adott játékos színének megfelelő led "körbefut" addig amíg a gomb nincs megnyomva
 6. Ellenőrzés, hogy a srága led és a "futó" led pozíciója a gombnyomás pillanatában egyezett-e
 7. (ha igen 1 pontal növekednek az adott játékos pontjai)
-8. 12 órai ledtől jobbra és balra a játékosok pontjaival egyenlő mennyiségő led világít pár másodpercig.
+8. 12 órai ledtől jobbra és balra a játékosok pontjaival egyenlő mennyiségű led világít pár másodpercig.
 9. Aki vezet annak a a "pontjai" villognak párat
 10. (ha döntetlen mindkettő pontjai villognak)
 11. Ha valaki pontjai elérik a 8-at nyert és az egész gyűrű az ő színével villog párat
-12. Ha még nincs nyertes újrakezdődik a 3. ponttól
+12. Ha még nincs nyertes játékosváltás újrakezdődik a 3. ponttól
--- cyclon_game_main/cyclon_game_main.ino
+++ cyclon_game_main/cyclon_game_main.ino
@@ -23,10 +23,64 @@ void update_previous_button_state(int update_with) {
 int read_button_state() {
     return digitalRead(2);
 }
-uint32_t getBlueColor() {  // Return a non-negative int type 
+
+uint32_t setRedColor() {  // Return a non-negative int type 
+    return pixels.Color(255,0,0);
+}
+
+uint32_t setGreenColor() {  // Return a non-negative int type 
+    return pixels.Color(0,255,0);
+}
+
+uint32_t setBlueColor() {  // Return a non-negative int type 
     return pixels.Color(0,0,255);
 }
 
+uint32_t setOffColor() {
+    return pixels.Color(0,0,0);
+}
+void show_palyers_colors() {
+    pixels.setBrightness(20);
+    pixels.setPixelColor(0, setGreenColor());
+    pixels.setPixelColor(8, setGreenColor());
+    pixels.fill(setBlueColor(), 1, 7);
+    pixels.fill(setRedColor(), 9, 15);
+    pixels.show();
+    delay(3000);
+}
+
+void switch_off_pixels(int fistPixel, int lastPixel) {
+    pixels.fill(setOffColor(), fistPixel, lastPixel);
+    pixels.show();
+}
+
+void blink_pixels(int fistPixel, int lastPixel){
+    for (int i = 0; i < 6; i++) {
+        pixels.fill(pixels.getPixelColor(fistPixel), fistPixel, lastPixel);
+        pixels.show();
+        delay(250);
+        switch_off_pixels(fistPixel,lastPixel);
+        delay(200);
+    }
+}
+
+void start_running_light() {
+    pixels.setPixelColor(0, setGreenColor());
+    int count = 0;
+    int clearPixelIndex = 0;
+    while (true) {
+      if (count == 16) {
+        count = 0;
+      }
+      switch_off_pixels(clearPixelIndex,clearPixelIndex);
+      pixels.setPixelColor(count, setBlueColor());
+      pixels.show();
+      delay(100);
+      count += 1;
+      clearPixelIndex = count-1;
+    }
+    
+}
 void setup() {
     #if defined (__ARVR_ATtiny85__)
         if(F_CPU == 16000000) clock_prescale_set(clock_div_1);
@@ -41,14 +95,19 @@ void loop() {
     int button_state = read_button_state();
     delay(10);  // Wait for debouncing (this is a crude filtering)
 
+
+    show_palyers_colors();
+    blink_pixels(1,7); //First player color blinks
+    switch_off_pixels(0,16);
+    start_running_light();
+    
     if (is_button_latched()) {  // Button state is different compared to last run
         update_previous_button_state(button_state);
-        
         Serial.println(previous_button_state);
-        
         if (is_button_released()) {
             for (int i=0; i<16; i++) {  // Set all pixel to blue and lit them up
-                pixels.setPixelColor(i, getBlueColor());
+                pixels.setPixelColor(i, setBlueColor());
+                pixels.setBrightness(1);
                 pixels.show();
                 delay(100);
             }
